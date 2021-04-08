7ed445e5fd75180b66202dd6f7ccefb6e8d7acde
Mostafa Waleed <mw18200615@gmail.com>
2021-04-08 20:39:48 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const API =
+  "https://api.themoviedb.org/3/movie/popular?api_key=26422703fa0de205ab53753b9502d57c";
+
+const IMG_API = "http://image.tmdb.org/t/p/w500";
+
+const app = document.getElementById("app");
+
+const getMovies = async () => {
+  const res = await fetch(API);
+  const data = await res.json();
+  const movies = await data.results;
+  console.log(movies);
+
+  await movies.map((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+        <img src=${IMG_API + movie.poster_path} loading="lazy" />
+        <div class="n-v"> 
+        <h4 class="title">${movie.title}</h4>
+        <span class="vote
+         ${movie.vote_average > 8 ? "green-v" : ""} 
+         ${movie.vote_average < 8 ? "yellow-v" : ""} 
+         ${movie.vote_average <= 6 ? "red-v" : ""} ">
+          ${movie.vote_average}
+          </span>
+        </div>
+        <div class="overview">
+            <h3>${movie.overview}</h3> 
+        </div>
+        `;
+    app.appendChild(div);
+  });
+};
+
+getMovies();
