ef83df6c5ae68f3a42a2f4cf3f8d54afb1139ca9
Mostafa Waleed <mw18200615@gmail.com>
2021-05-03 03:38:25 +0300

    fixed trailer not showing in the search bug

--- search.js
+++ search.js
@@ -15,25 +15,43 @@ form.addEventListener("submit", (e) => {
     const movies = await data.results;
     console.log(movies);
     if (movies.length > 0) {
-      movies.map((movie) => {
-        const div = document.createElement("div");
-        div.classList.add("card");
-        div.innerHTML = `
-            <img src=${IMG_API + movie.poster_path} loading="lazy" />
-            <div class="n-v"> 
-            <h4 class="title">${movie.title}</h4>
-            <span class="vote
-             ${movie.vote_average >= 8 ? "green-v" : ""} 
-             ${movie.vote_average < 8 ? "yellow-v" : ""} 
-             ${movie.vote_average <= 5 ? "red-v" : ""} ">
-              ${movie.vote_average}
-              </span>
-            </div>
-            <div class="overview">
-                <h3>${movie.overview}</h3> 
-            </div>
-            `;
-        app.appendChild(div);
+      await movies.map((movie) => {
+        const getMovieswT = async () => {
+          const tres = await fetch(TRAILER_API_F + movie.id + TRAILER_API_S);
+          const tdata = await tres.json();
+          const tmovies = await tdata.results;
+          console.log(tmovies[0].key);
+          const tYTlink = `https://youtube.com/embed/${tmovies[0].key}`;
+
+          const div = document.createElement("div");
+
+          div.classList.add("card");
+          div.innerHTML = `
+          <div class="img">
+          <img src=${
+            IMG_API + movie.poster_path
+          } loading="lazy" class="movie-poster"  />
+          <a href="${tYTlink}" data-lity>
+           <img src="assets/play-button.svg" class="trailer-button">
+           </a>
+          </div>
+          <div class="n-v"> 
+          <h4 class="title">${movie.title}</h4>
+          <span class="vote
+           ${movie.vote_average >= 8 ? "green-v" : ""} 
+           ${movie.vote_average < 8 ? "yellow-v" : ""} 
+           ${movie.vote_average <= 5 ? "red-v" : ""} ">
+            ${movie.vote_average}
+            </span>
+          </div>
+          <div class="overview">
+              <h3>${movie.overview}</h3> 
+          </div>
+          `;
+          app.appendChild(div);
+        };
+
+        getMovieswT();
       });
     } else {
       const div = document.createElement("div");
